b65b980b4b6381927d8bdc6982f913296500a428
[akif ödül] <[akifodul@hotmail.com]>
2023-07-02 12:34:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function greet(name, lastName) {
+    console.log('hello ' + name + ' ' + lastName);
+}
+
+greet('Akif', 'Ödül');
+
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+
+console.log(number);
+
+console.log(square(3));
